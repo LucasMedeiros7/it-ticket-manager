edc18bb8daba60a15a83fd85b5078b5314d87813
LucasMedeiros7 <lucaasmedeiros7@gmail.com>
2023-12-29 13:42:16 -0300

    feat: add update and listById methods in TicketRepository

--- src/application/repositories/TicketRepository.ts
+++ src/application/repositories/TicketRepository.ts
@@ -3,4 +3,6 @@ import { Ticket } from '../entities/Ticket'
 export interface TicketRepository {
   save(ticket: Ticket): Promise<void>
   listAll(): Promise<Ticket[]>
+  listById(id: string): Promise<Ticket>
+  update(ticket: Ticket): Promise<void>
 }
--- src/infra/repositories/FakeTicketRepository.ts
+++ src/infra/repositories/FakeTicketRepository.ts
@@ -11,4 +11,24 @@ export class FakeTicketRepository implements TicketRepository {
   async listAll(): Promise<Ticket[]> {
     return this.tickets
   }
+
+  async listById(id: string): Promise<Ticket | undefined> {
+    return this.tickets.find((ticket) => ticket.getTicketId() === id)
+    /* 
+      for (let i = 0; i < this.tickets.length; i++) {
+        const ticketId = this.tickets[i].getTicketId()
+        if (ticketId === id) {
+          return this.tickets[i]
+        }
+      } 
+     */
+  }
+
+  async update(updatedTicket: Ticket): Promise<void> {
+    const tickets = this.tickets.filter(
+      (ticket) => ticket.getTicketId() !== updatedTicket.getTicketId(),
+    )
+    tickets.push(updatedTicket)
+    this.tickets = tickets
+  }
 }
