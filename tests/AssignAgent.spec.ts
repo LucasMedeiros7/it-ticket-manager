967983bf652e388b8c342a21295701e2936e6d88
LucasMedeiros7 <lucaasmedeiros7@gmail.com>
2023-12-29 13:43:53 -0300

    test: assignAgent and createTicket tests

---
+++ tests/AssignAgent.spec.ts
@@ -0,0 +1,40 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { CreateTicket } from '../src/application/usecases/CreateTicket'
+import { FakeTicketRepository } from '../src/infra/repositories/FakeTicketRepository'
+import { ListTicketById } from '../src/application/usecases/ListTicketById'
+import { AssignAgent } from '../src/application/usecases/AssignAgent'
+import { CreateTicketDTO } from '../src/application/dtos/CreateTicketDTO'
+
+describe('Assign Agent', () => {
+  let fakeRepository: FakeTicketRepository
+
+  let createTicket: CreateTicket
+  let listTicketById: ListTicketById
+  let assignAgent: AssignAgent
+
+  let input: CreateTicketDTO
+
+  beforeEach(() => {
+    fakeRepository = new FakeTicketRepository()
+    createTicket = new CreateTicket(fakeRepository)
+    listTicketById = new ListTicketById(fakeRepository)
+    assignAgent = new AssignAgent(fakeRepository)
+
+    input = {
+      subject: 'Problem in the testing environment',
+      description:
+        "Can't access the testing environment with the AWS console using the provided account!",
+      priority: 'High',
+      customer: 'Lucas Medeiros',
+    }
+  })
+
+  it('Deve ser possivel atribuir um Ticket a um Agent', async () => {
+    const ticket = await createTicket.execute(input)
+
+    assignAgent.execute(ticket, 'Anderson Gonhi')
+    const output = await listTicketById.execute(ticket.getTicketId())
+
+    expect(output.getAssignedAgent()).toBe('Anderson Gonhi')
+  })
+})
--- tests/CreateTicket.spec.ts
+++ tests/CreateTicket.spec.ts
@@ -1,12 +1,12 @@
 import { describe, expect, it, beforeEach } from 'vitest'
-import { Input } from '../src/application/entities/Ticket'
 import { CreateTicket } from '../src/application/usecases/CreateTicket'
 import { FakeTicketRepository } from '../src/infra/repositories/FakeTicketRepository'
+import { CreateTicketDTO } from '../src/application/dtos/CreateTicketDTO'
 
 describe('Create Ticket Use Case', () => {
   let fakeRepository: FakeTicketRepository
   let createTicket: CreateTicket
-  let input: Input
+  let input: CreateTicketDTO
 
   beforeEach(() => {
     fakeRepository = new FakeTicketRepository()
@@ -24,19 +24,19 @@ describe('Create Ticket Use Case', () => {
   it('should be able to create a ticket and save it in the database', async () => {
     const output = await createTicket.execute(input)
     const savedTicket = await fakeRepository.listAll()
-    expect(savedTicket[0].ticketId).toBe(output.ticketId)
+    expect(savedTicket[0].getTicketId()).toBe(output.getTicketId())
   })
 
   it('should assign the ticket to the specified agent', async () => {
     await createTicket.execute(input)
     const savedTicket = await fakeRepository.listAll()
-    expect(savedTicket[0].assignedAgent).toBe(input.assignedAgent)
+    expect(savedTicket[0].getAssignedAgent()).toBe(input.assignedAgent)
   })
 
   it('should set the default status to "Pending" for a new ticket', async () => {
     await createTicket.execute(input)
     const savedTicket = await fakeRepository.listAll()
-    expect(savedTicket[0].status).toBe('Pending')
+    expect(savedTicket[0].getStatus()).toBe('Pending')
   })
 
   it('should not allow creating a ticket without a subject', async () => {
